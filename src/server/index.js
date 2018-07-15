f291cba5f9d3b8a4e9741c5a50716f8c6052db70
Berkay Barlas <berkay.barlas@C02VN686HTD6.local>
2018-07-15 13:32:09 +0300

    fixed server side connection

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
-const server = require('http').Server(app)
-const io = module.exports.io = require('socket.io')(server)
+const http = require("http")
+const socketIo = require("socket.io")
+const server = http.createServer(app)
+server.listen(3000)
+const io = module.exports.io = socketIo(server)
 
-const PORT = process.env.PORT || 3231
+const PORT = process.env.PORT || 3232
 
 const SocketManager = require('./SocketManager')
 
