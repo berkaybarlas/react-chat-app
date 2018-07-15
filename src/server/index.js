48b43a4d87d81e8bb2b2089156207daa97610b0b
berkaybarlas <fpberkay@gmail.com>
2018-07-15 18:10:39 +0300

    color changed purple to calm red

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ const socketIo = require("socket.io")
 const server = http.createServer(app)
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 
 server.listen(PORT,()=>{
 	console.log("Listening to port: " + PORT )
